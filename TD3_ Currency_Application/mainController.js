02aa3c617041bbc8ea8d4d8cace354d358f00b71
PINOTMorine <mlle.ocatopus@hotmail.com>
2017-03-03 11:01:57 +0100

    TD3- Currency application - version 2
    Fait : les listes déroulantes
    
    Reste à faire : la convertion

--- TD3_ Currency_Application/mainController.js
+++ TD3_ Currency_Application/mainController.js
@@ -3,19 +3,31 @@
  */
 
 angular.module("TD3").controller("MainController",['$http', function($http) {
-    //var self=this;
+    var self=this;
+    var from="";
+    var to="";
 
     $http.get('app/data/currencymap.json').
     then(function(response) {
             self.currencies = response.data;
+            self.from=self.currencies["EUR"];
+            self.to=self.currencies["USD"];
+
         },
         function(response) {
             console.log("Erreur avec le statut Http : "+response.status);
         });
 
-    /*$http.jsonp('https://free.currencyconverterapi.com/api/v3/convert?compact=y&q='+from.code+'_'+to.code, {jsonpCallbackParam: 'callback'})
-        .then(function(response) {
-            self.result=response.data[self.from.code+'_'+self.to.code].val;
+    this.convertir=function(){
+        $http.jsonp('https://free.currencyconverterapi.com/api/v3/convert?compact=y&q='+from.code+'_'+to.code, {jsonpCallbackParam: 'callback'})
+            .then(function(response) {
+                self.result=response.data[self.from.code+'_'+self.to.code].val;
 
-        });*/
+            });
+    }
+    this.swap=function(){
+        var inter=self.from;
+        self.from=self.to;
+        self.to=inter;
+    }
 }]);
