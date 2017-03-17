25548ed51677a56385f699c75261d0da08883b35
PINOTMorine <mlle.ocatopus@hotmail.com>
2017-03-17 10:12:43 +0100

    TD5_Routage_Cours (partie 5 à fin)
    Réalisation de la fin du cours

--- TD5_Routage_Cours/app/controllers.js
+++ TD5_Routage_Cours/app/controllers.js
@@ -1,7 +1,13 @@
 /**
  * Created by Morine on 17/03/2017.
  */
-angular.module("sampleApp").controller("RouteController",["$routeParams",function($routeParams){
+angular.module("sampleApp").controller("RouteController",["$routeParams","code",function($routeParams,code){
     this.content1="Contenu du template de route1";
     this.params=$routeParams;
+    this.code=code;
 }]);
+angular.module("sampleApp").factory("code", function() {
+    return {
+        value:"noCode"
+    }
+});
